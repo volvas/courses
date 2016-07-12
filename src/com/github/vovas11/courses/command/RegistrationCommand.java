44d9d604604b41f8c8463883af243821b4445a8f
Vova <vova@example.com>
2016-07-12 18:13:15 +0200

    Add comment into RegistrationCommand.java

--- src/com/github/vovas11/courses/command/RegistrationCommand.java
+++ src/com/github/vovas11/courses/command/RegistrationCommand.java
@@ -9,8 +9,8 @@ import com.github.vovas11.courses.dao.*;
  * sends it to the database via {@code UserDao}
  * 
  * @author vovas11
- * @see com.github.vovas11.courses.dao.DaoFactory
- * @see com.github.vovas11.courses.dao.UserDao
+ * @see DaoFactory
+ * @see UserDao
  */
 public class RegistrationCommand implements Command {
     /**
@@ -24,18 +24,19 @@ public class RegistrationCommand implements Command {
      */
     @Override
     public String execute(HttpServletRequest request) {
-	// gets parameters form the HTTP request
+	
+	/* gets parameters from the HTTP request */
 	String login = request.getParameter("login");
 	String password = request.getParameter("password");
 	String firstName = request.getParameter("firstname");
 	String lastName = request.getParameter("lastname");
 	String department = request.getParameter("department");
 	
-	// gets the link to the DaoFactory and UserDao
+	/* gets the link to the DaoFactory and UserDao */
 	DaoFactory daoFactory = DaoFactory.getInstance();
 	UserDao users = daoFactory.getUserDao();
-	// creates the new instance of the User and fills in fields
 	
+	/* creates the new instance of the User and fills in fields */
 	User user = new User();
 	user.setLogin(login);
 	user.setPassword(password);
@@ -43,12 +44,12 @@ public class RegistrationCommand implements Command {
 	user.setLastName(lastName);
 	user.setDepartment(department);
 	
-	// checks if the user exists
+	/* checks if the user (field 'login') exists and if yes returns back to the registration
+	 * page, if no inserts new user into database and proceeds to the login page*/
 	if (users.isLoginExist(user)) {
-	    // returns back to the registration page if the user exists
 	    return "/registration.html";
+	    
 	} else {
-	    // returns the name of the login page to proceed
 	    users.insert(user);
 	    return "/login.html";
 	}
