97582c689398fa80468cb86ebea083e3aa7135b1
Vova <vova@example.com>
2016-07-16 19:34:37 +0200

    Add database tables dumps

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# Courses for students
+Courses for students
+====================
+  
 WEB Project for training purpose using web server with Servlets, JSP, Connection pool, Java code, design patterns etc.
-
-===========
+  
 **Description**  
-System of optional courses. There is a list of courses, each course is assigned to a lecturer. Student subscribes to one or more courses. Registration data is stored. After course completion lecturer evaluates it with a mark and data stores in the archive.  
+System of optional **courses**. There is a list of courses, each **course** is assigned to a **lecturer**. **Student** subscribes to one or more courses. Registration data is stored. After course completion lecturer evaluates it with a mark and data stores in the **archive**.  
   
 **General requirements to the project**  
-
 Build WEB system having the following functions:  
 1. Build classes describing entities of the data domain  
 2. Names of classes and methods should reflect their functionality and classes should have correct hierarhy  
 3. Code should satisfy Java Code Convention  
 4. Information about data domain should be kept in a database. API JDBC with connection pool (standard or custom) should be used for access into the database. MySQL or Derby are recommended as a database  
-5. The application should support cyrillic (be multilangual), including information in DB  
+5. The application should support cyrillic (be multilingual), including information in DB  
 6. Architecture of application should correspond to template Model-View-Controller  
 7. During logic implementation GoF templates like Factory Method, Command, Builder, Strategy, State, Observer etc are to be used  
 8. Implement functionality that is mentioned in the task using servlets and JSP  
@@ -20,5 +20,13 @@ Build WEB system having the following functions:
 10. During business logic development sessions and filters are to be used  
 11. Implement event logging i.e. information about arising exceptions and events in the system is to be handled with Log4j  
 12. The application should contain comments.  
+  
+**Installation**
+Web-Server: Apache Tomcat/8.0.30
+Database: MySQL Community Server 5.7.12
+  
+**Restrictions**  
+  
 
-**Restrictions**
+**Documentation**
+./doc/index.html
---
+++ sql/dbcoursetest2_students.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dbcoursetest2
+-- ------------------------------------------------------
+-- Server version	5.7.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Byron','Bradley','byronbradley','byronbradley','Faculty of Economics'),(2,'Silva','Armenta','silvaarmenta','silvaarmenta','Faculty of Economics'),(3,'Joyce','Maloney','joycemaloney','joycemaloney','Faculty of History'),(4,'John','Smith','johnsmith','johnsmith','Faculty of Ecology');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-16 19:31:40
