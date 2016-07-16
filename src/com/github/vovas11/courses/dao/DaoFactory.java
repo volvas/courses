80eb5e6f63de61c7e2ab0896b2e1a18b87caee91
Vova <vova@example.com>
2016-07-16 16:29:09 +0200

    Add comments into DaoFactory.java

--- src/com/github/vovas11/courses/dao/DaoFactory.java
+++ src/com/github/vovas11/courses/dao/DaoFactory.java
@@ -1,19 +1,27 @@
-/**
- * 
- * 
- * 
- * 
- */
 package com.github.vovas11.courses.dao;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+/**
+ * {@code DaoFactory} is a factory (design pattern) that creates instances
+ * of the data access objects (DAO).
+ * 
+ * @author vovas11
+ * @see CourseDao
+ * @see UserDao
+ */
 public class DaoFactory {
+    
+    /* stores the link to the single instance (Singleton) */
     static DaoFactory factory = new DaoFactory();
+    
+    /* stores the link to the common data source */
     DataSource datasrc;
     
+    /* forbids the instantiation of the class outside and receives the link to datasource
+     * by Initial Context */
     private DaoFactory() {
 	try {
 	    InitialContext initContext = new InitialContext();
@@ -23,14 +31,28 @@ public class DaoFactory {
 	}
     }
     
+    /**
+     * Returns to the link to single instance of the DaoFactory class
+     * @return link to single instance of the class
+     */
     public static DaoFactory getInstance() {
 	return factory;
     }
     
+    /**
+     * Creates the instance of the UserDao class and returns the link
+     * 
+     * @return link to the instance of UserDao
+     */
     public UserDao getUserDao() {
-	UserDao users = new UserDao(datasrc);
-	return users;
+	return new UserDao(datasrc);
     }
+    
+    /**
+     * Creates the instance of the CourseDao class and returns the link
+     * 
+     * @return link to the instance of CourseDao
+     */
     public CourseDao getCourseDao() {
 	return new CourseDao(datasrc);
     }
