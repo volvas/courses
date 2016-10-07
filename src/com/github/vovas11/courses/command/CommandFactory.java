67940008a3b0a2e1aba7780dd9c0eced6707f2cd
Vova <vova@example.com>
2016-10-07 16:37:59 +0200

    Change the IDE and update CommandFactoy.java
    Update some comments
    Added modificator private for local fields

--- src/com/github/vovas11/courses/command/CommandFactory.java
+++ src/com/github/vovas11/courses/command/CommandFactory.java
@@ -8,24 +8,23 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * {@code CommandFactory} is a factory (design pattern) that stores all commands
  * needed for handling user forms and returns one of commands when it needed
- * 
+ *
  * @author vovas11
  * @see Command
  */
 public class CommandFactory {
 
     /* represents one instance of the CommandFactory (Singleton) */
-    static CommandFactory commandFactory = new CommandFactory();
+    private static CommandFactory commandFactory = new CommandFactory();
 
     /* stores all commands */
-    Map<String, Command> commandMap = new HashMap<>();
+    private Map<String, Command> commandMap = new HashMap<>();
 
-    /* Private Constructor does not allow creating instances except by this class */
     private CommandFactory() {
-	commandMap.put("registration", new RegistrationCommand());
-	commandMap.put("login", new LoginCommand());
-	commandMap.put("courseselect", new CourseSelectCommand());
-	commandMap.put("logout", new LogoutCommand());
+        commandMap.put("registration", new RegistrationCommand());
+        commandMap.put("login", new LoginCommand());
+        commandMap.put("courseselect", new CourseSelectCommand());
+        commandMap.put("logout", new LogoutCommand());
     }
 
     /**
@@ -33,17 +32,17 @@ public class CommandFactory {
      * @return the link to the {@code CommandFactory} instance
      */
     public static CommandFactory getInstance() {
-	return commandFactory;
+        return commandFactory;
     }
 
     /**
      * Delivers an object of the command the name of which is sent inside the HTTP request
-     * 
+     *
      * @param   request   HTTP request from the servlet
      * @return the single instance of the {@code Command}
      */
     public Command getCommand(HttpServletRequest request) {
-	String value = request.getParameter("ok");
-	return commandMap.get(value);
+        String value = request.getParameter("ok");
+        return commandMap.get(value);
     }
 }
