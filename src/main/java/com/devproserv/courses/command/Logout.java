ad924fb33560a928bdfb987979b9b55e3b1c7145
Vladimir <vovas11@gmail.com>
2018-04-08 16:49:06 +0200

    Add logging

--- src/main/java/com/devproserv/courses/command/Logout.java
+++ src/main/java/com/devproserv/courses/command/Logout.java
@@ -1,5 +1,8 @@
 package com.devproserv.courses.command;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -10,10 +13,14 @@ import static com.devproserv.courses.config.MainConfig.HOME_PAGE;
  *
  */
 public class Logout implements Command {
+
+    private static final Logger logger = LogManager.getLogger(Logout.class.getName());
+
     @Override
     public String path(HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.invalidate();
+        logger.info("User logged out.");
         return HOME_PAGE;
     }
 }
