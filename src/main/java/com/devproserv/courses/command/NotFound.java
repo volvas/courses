8b045462d82df13a5c6da462cfaea58bfcd42293
Vladimir <vovas11@gmail.com>
2018-04-08 16:36:45 +0200

    Add logging

--- src/main/java/com/devproserv/courses/command/NotFound.java
+++ src/main/java/com/devproserv/courses/command/NotFound.java
@@ -1,5 +1,8 @@
 package com.devproserv.courses.command;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 
 import static com.devproserv.courses.config.MainConfig.NOT_FOUND_PAGE;
@@ -8,8 +11,12 @@ import static com.devproserv.courses.config.MainConfig.NOT_FOUND_PAGE;
  * {@code NotFound} represents
  */
 public class NotFound implements Command {
+
+    private static final Logger logger = LogManager.getLogger(NotFound.class.getName());
+
     @Override
     public String path(HttpServletRequest request) {
+        logger.warn("Command not found!");
         return NOT_FOUND_PAGE;
     }
 }
