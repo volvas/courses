9d2a81d3917b8e436501ad907aeda45d48c26818
Vladimir <vovas11@gmail.com>
2018-04-08 16:46:42 +0200

    Add test for NotFound command

---
+++ src/test/java/com/devproserv/courses/command/NotFoundTest.java
@@ -0,0 +1,35 @@
+package com.devproserv.courses.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.devproserv.courses.config.MainConfig.NOT_FOUND_PAGE;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Contains unit-tests to check functionality of {@link NotFound} class
+ *
+ */
+public class NotFoundTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    private Command notFound;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        notFound = new NotFound();
+    }
+
+    @Test
+    public void testExecuteCommandOk() {
+        String path = notFound.path(request);
+        assertEquals("Should be equal to " + NOT_FOUND_PAGE, NOT_FOUND_PAGE, path);
+    }
+}
