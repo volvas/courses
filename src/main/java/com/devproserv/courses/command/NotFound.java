39af8f956e31706233e9bcf9270cd1b13e39c56d
Vladimir <vovas11@gmail.com>
2018-04-11 13:56:11 +0200

    Remove logger from NotFound command

--- src/main/java/com/devproserv/courses/command/NotFound.java
+++ src/main/java/com/devproserv/courses/command/NotFound.java
@@ -1,23 +1,16 @@
 package com.devproserv.courses.command;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import javax.servlet.http.HttpServletRequest;
 
 import static com.devproserv.courses.config.MainConfig.NOT_FOUND_PAGE;
 
 /**
- * {@code NotFound} represents a non-existing command
+ * Represents a non-existing command
  *
  */
 public class NotFound implements Command {
-
-    private static final Logger logger = LogManager.getLogger(NotFound.class.getName());
-
     @Override
     public String path(HttpServletRequest request) {
-        logger.warn("Command not found!");
         return NOT_FOUND_PAGE;
     }
 }
