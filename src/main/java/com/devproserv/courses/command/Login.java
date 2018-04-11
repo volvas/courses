184e02c0ea9a642a684b07ff86b81499a0a015ad
Vladimir <vovas11@gmail.com>
2018-04-11 14:02:28 +0200

    Delegate work to several classes

--- src/main/java/com/devproserv/courses/command/Login.java
+++ src/main/java/com/devproserv/courses/command/Login.java
@@ -1,119 +1,26 @@
 package com.devproserv.courses.command;
 
-import java.util.List;
+import com.devproserv.courses.servlet.AppContext;
+import com.devproserv.courses.temp.LoginForm;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import com.devproserv.courses.util.Validation;
-import com.devproserv.courses.controller.AppContext;
-import com.devproserv.courses.model.Course;
-import com.devproserv.courses.model.User;
-import com.devproserv.courses.dao.UserDao;
-import com.devproserv.courses.dao.CourseDao;
-
-import static com.devproserv.courses.config.MainConfig.LOGIN_PAGE;
-import static com.devproserv.courses.config.MainConfig.STUDENT_PAGE;
-import static com.devproserv.courses.config.MainConfig.LECTURER_PAGE;
 /**
- * {@code Login} handles access for the existing user. After user input valid
- * credentials he/she gets the own page
- * 
- * @author vovas11
- * @see UserDao
- * @see CourseDao
- * 
+ * Treats data from login web form
+ *
  */
 public class Login implements Command {
     
-    /** Injection of the main app manager */
     private AppContext appContext;
 
 
     public Login(AppContext appContext) {
         this.appContext = appContext;
     }
-    
-    /**
-     * Validates user name and password, checks if credentials exist in database
-     *
-     * @param request HTTP request
-     * @return own page to client if credentials are valid or the same page
-     */
+
+
     @Override
     public String path(HttpServletRequest request) {
-
-        /* gets parameters from the HTTP request */
-        String login = request.getParameter("login");
-        String password = request.getParameter("password");
-
-        /* checks if login and password are valid */
-        String validResponse = Validation.checkCredentials(login, password);
-
-        if (!validResponse.equals("ok")) {
-            request.setAttribute("message", validResponse);
-            return LOGIN_PAGE;
-        }
-
-        UserDao userDao = appContext.getUserDao();
-        /* checks if the user with entered credentials exists in the database */
-        if (!userDao.userExists(login, password)) {
-            validResponse = "Wrong username or password! Try again!";
-            request.setAttribute("message", validResponse);
-            return LOGIN_PAGE;
-        }
-        
-        /* gets a link to instance of User class and gets all data for the user */
-        User user = userDao.getUser(login, password);
-        /* gets the link to the current session or creates new one and attaches the user to the session */
-        HttpSession session = request.getSession(); // TODO add login.jsp filter to check validated session
-        session.setAttribute(session.getId(), user);
-        
-        /* Define a page where the user will be redirected according to user role */
-        String path = LOGIN_PAGE;
-        switch (user.getRole()) {
-        case STUD:
-            prepareStudentPage(request, user);
-            path = STUDENT_PAGE;
-            break;
-        case LECT:
-            prepareLecturerPage(request, user);
-            path = LECTURER_PAGE;
-            break;
-        case ADMIN:
-            request.setAttribute("message", "This account is not accessible!");
-            path = LOGIN_PAGE;
-            break;
-        }
-        return path;
-    }
-
-    /**
-     * Prepares data to be displayed on the own student's page and attaches the data
-     * to HTTP request. Then JSP servlet will handle this request.
-     * 
-     * @param request HTTP request
-     * @param user user
-     */
-    private void prepareStudentPage(HttpServletRequest request, User user) {
-        CourseDao courses = appContext.getCourseDao();
-        /* prepares the data for the JSP page */
-        request.setAttribute("student", user);
-        /* gets subscribed courses and available courses */
-        List<Course> subscribedCourses = courses.getSubscribedCourses(user);
-        request.setAttribute("subscrcourses", subscribedCourses);
-        List<Course> availableCourses = courses.getAvailableCourses(user);
-        request.setAttribute("courses", availableCourses);
-    }
-    
-    /**
-     * Prepares data to be displayed on the own lecturer's page and attaches the data
-     * to HTTP request. Then JSP servlet will handle this request.
-     * 
-     * @param request HTTP request
-     * @param user user
-     */
-    private void prepareLecturerPage(HttpServletRequest request, User user) {
-        // TODO
+        return new LoginForm(appContext, request).validate();
     }
 }
--- src/test/java/com/devproserv/courses/command/LoginTest.java
+++ src/test/java/com/devproserv/courses/command/LoginTest.java
@@ -1,27 +1,15 @@
 package com.devproserv.courses.command;
 
-import com.devproserv.courses.controller.AppContext;
-import com.devproserv.courses.dao.CourseDao;
-import com.devproserv.courses.dao.UserDao;
-import com.devproserv.courses.model.User;
-import com.devproserv.courses.model.User.Role;
+import com.devproserv.courses.servlet.AppContext;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.Collections;
 
-import static com.devproserv.courses.config.MainConfig.LECTURER_PAGE;
 import static com.devproserv.courses.config.MainConfig.LOGIN_PAGE;
-import static com.devproserv.courses.config.MainConfig.STUDENT_PAGE;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 
 /**
  * Contains unit-tests to check functionality of {@link Login} class
@@ -32,14 +20,6 @@ public class LoginTest {
     private AppContext appContext;
     @Mock
     private HttpServletRequest request;
-    @Mock
-    private HttpSession session;
-    @Mock
-    private UserDao userDao;
-    @Mock
-    private CourseDao courseDao;
-    @Mock
-    private User user;
 
     private Login login;
 
@@ -48,79 +28,12 @@ public class LoginTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         login = new Login(appContext);
-
-        when(request.getParameter("login")).thenReturn("Login");
-        when(request.getParameter("password")).thenReturn("Password");
-        when(request.getSession()).thenReturn(session);
-        doNothing().when(request).setAttribute(anyString(), anyString());
-
-        doNothing().when(session).setAttribute(anyString(), any());
-
-        when(appContext.getUserDao()).thenReturn(userDao);
-        when(appContext.getCourseDao()).thenReturn(courseDao);
-
-        when(userDao.getUser("Login", "Password")).thenReturn(user);
-
-        when(courseDao.getSubscribedCourses(user)).thenReturn(Collections.emptyList());
-        when(courseDao.getAvailableCourses(user)).thenReturn(Collections.emptyList());
     }
-    
-    @Test
-    public void testExecuteCommandOkStudent() {
-        when(userDao.userExists("Login", "Password")).thenReturn(true);
 
-        when(user.getRole()).thenReturn(Role.STUD);
-        
-        String page = login.path(request);
-        assertEquals("Should be equal to " + STUDENT_PAGE, STUDENT_PAGE, page);
-    }
-    
-    @Test
-    public void testExecuteCommandOkLecturer() {
-        when(userDao.userExists("Login", "Password")).thenReturn(true);
 
-        when(user.getRole()).thenReturn(Role.LECT);
-        
-        String page = login.path(request);
-        assertEquals("Should be equal to " + LECTURER_PAGE, LECTURER_PAGE, page);
-    }
-    
     @Test
-    public void testExecuteCommandOkAdmin() {
-        when(userDao.userExists("Login", "Password")).thenReturn(true);
-
-        when(user.getRole()).thenReturn(Role.ADMIN);
-        
-        String page = login.path(request);
-        assertEquals("Should be equal to " + LOGIN_PAGE, LOGIN_PAGE, page);
-    }
-    
-    @Test
-    public void testExecuteCommandInvalidLogin() {
-        when(request.getParameter("login")).thenReturn("contains space");
-        when(request.getParameter("password")).thenReturn("Password");
-        
-        String page = login.path(request);
-        assertEquals("Should be equal to " + LOGIN_PAGE, LOGIN_PAGE, page);
-    }
-    
-    @Test
-    public void testExecuteCommandInvalidPassword() {
-        when(request.getParameter("login")).thenReturn("Login");
-        when(request.getParameter("password")).thenReturn("");
-        
-        String page = login.path(request);
-        assertEquals("Should be equal to " + LOGIN_PAGE, LOGIN_PAGE, page);
-    }
-    
-    @Test
-    public void testExecuteCommandUserDoesNotExist() {
-        when(request.getParameter("login")).thenReturn("Login");
-        when(request.getParameter("password")).thenReturn("Password");
-
-        when(userDao.userExists("Login", "Password")).thenReturn(false);
-        
-        String page = login.path(request);
-        assertEquals("Should be equal to " + LOGIN_PAGE, LOGIN_PAGE, page);
+    public void testPathOk() {
+        String path = login.path(request);
+        assertEquals(LOGIN_PAGE, path);
     }
 }
