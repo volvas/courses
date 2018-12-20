fe8e4abc418ebd0a8d5f48e8f6957ea66e4fea2c
Vladimir <14625997+volvas@users.noreply.github.com>
2018-12-20 16:08:17 +0100

    Refactor partly package "form" in accordance to Qulice

--- src/main/java/com/devproserv/courses/form/CourseHandling.java
+++ src/main/java/com/devproserv/courses/form/CourseHandling.java
@@ -1,24 +1,74 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Vladimir
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.devproserv.courses.form;
 
+import com.devproserv.courses.config.MainConfig;
 import com.devproserv.courses.model.Course;
 import com.devproserv.courses.model.Student;
-
 import javax.servlet.http.HttpServletRequest;
 
-import static com.devproserv.courses.config.MainConfig.STUDENT_PAGE;
-
+/**
+ * Prepares request for further forwarding.
+ *
+ * @since 1.0.0
+ */
 abstract class CourseHandling {
-
+    /**
+     * Returns course ID parameter.
+     * @return Course ID
+     */
     abstract String courseIdParameter();
 
+    /**
+     * Returns error message.
+     * @return Error message
+     */
     abstract String errorMessageParameter();
 
+    /**
+     * Changes entry.
+     *
+     * @param course Course instance
+     * @param user User instance
+     */
     abstract void changeEntry(Course course, Student user);
 
-    String path(Course course, Student user, HttpServletRequest request) {
+    /**
+     * Returns a string containing the path.
+     * @param course Course instance
+     * @param user User instance
+     * @param request HTTP request
+     * @return Path
+     */
+    String path(
+        final Course course, final Student user,
+        final HttpServletRequest request
+    ) {
         // TODO check the number of the course is not already subscribed
-        changeEntry(course, user);
+        this.changeEntry(course, user);
         user.prepareJspData(request);
-        return STUDENT_PAGE;
+        return MainConfig.STUDENT_PAGE;
     }
 }
--- src/main/java/com/devproserv/courses/form/EnrollForm.java
+++ src/main/java/com/devproserv/courses/form/EnrollForm.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Vladimir
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.devproserv.courses.form;
 
 import com.devproserv.courses.model.Course;
@@ -5,7 +28,6 @@ import com.devproserv.courses.model.Student;
 import com.devproserv.courses.servlet.AppContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -14,7 +36,7 @@ import static com.devproserv.courses.config.MainConfig.STUDENT_PAGE;
 
 public class EnrollForm implements Form {
 
-    private static final Logger logger = LogManager.getLogger(EnrollForm.class.getName());
+    private static final Logger LOGGER = LogManager.getLogger(EnrollForm.class.getName());
 
     private final AppContext appContext;
     private final CourseHandling courseHandling;
@@ -48,7 +70,7 @@ public class EnrollForm implements Form {
 
     private String invalidPath(final Validation validation,
                                final HttpServletRequest request) {
-        logger.info("Invalid credentials for login " + user.getLogin());
+        LOGGER.info("Invalid credentials for login " + user.getLogin());
         request.setAttribute(courseHandling.errorMessageParameter(),
                 validation.errorMessage());
         user.prepareJspData(request);
